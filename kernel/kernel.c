fb2d6c47eae371c2fcf25c8fd8cdda18eef2cc03
Christopher Smith <chris@hichris.com>
2016-02-11 15:17:51 -0500

    Moved the VGA library to it's own file. Deprecated bare-bones. Changed folder struture.

---
+++ kernel/kernel.c
@@ -0,0 +1,32 @@
+#if !defined(__cplusplus)
+#include <stdbool.h> /* C doesn't have booleans by default. */
+#endif
+#include <stddef.h>
+#include <stdint.h>
+ 
+/* Check if the compiler thinks we are targeting the wrong operating system. */
+#if defined(__linux__)
+#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#endif
+ 
+/* This tutorial will only work for the 32-bit ix86 targets. */
+#if !defined(__i386__)
+#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#endif
+ 
+#if defined(__cplusplus)
+extern "C" /* Use C linkage for kernel_main. */
+#endif
+
+#include "../modules/screen/vga.h"
+
+void kernel_main() {
+	/* Initialize terminal interface */
+	terminal_initialize();
+ 
+	/* Since there is no support for newlines in terminal_putchar
+         * yet, '\n' will produce some VGA specific character instead.
+         * This is normal.
+         */
+	terminal_writestring("Hello, kernel World! YES I INCLUDED SOMETHINGff\nffffff j\nfkdjs lf fjdsklf sjkl fdjskl fdjskl fj dksl jfklds\n");
+}
