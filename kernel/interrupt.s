53b9d3d76cf9fd435ae065a12e8c514873f5e70e
chris <chris@hichris.com>
2016-02-29 23:44:27 -0500

    Fixed backwards mov instructions..still doesn't work :/

--- kernel/interrupt.s
+++ kernel/interrupt.s
@@ -35,17 +35,17 @@ interrupt:
 
 .global gdt_flush
 gdt_flush:
-    #mov %eax, (%esp,4)  # Get the pointer to the GDT, passed as a parameter.
-    lgdt (%eax,4)        # Load the new GDT pointer
+    movl 4(%esp), %eax  # Get the pointer to the GDT, passed as a parameter.
+    lgdt (%eax)        # Load the new GDT pointer
 
-    mov %ax, 0x10      # 0x10 is the offset in the GDT to our data segment
-    mov %ds, %ax        # Load all data segment selectors
-    mov %es, %ax
-    mov %fs, %ax
-    mov %gs, %ax
-    mov %ss, %ax
+    mov 0x10, %ax      # 0x10 is the offset in the GDT to our data segment
+    mov %ax, %ds         # Load all data segment selectors
+    mov %ax, %es
+    mov %ax, %fs
+    mov %ax, %gs
+    mov %ax, %ss
 
-    jmp .flush   # 0x08 is the offset to our code segment: Far jump!
-    .skip 0x08
-.flush:
+    jmp $0x08, $flush   # 0x08 is the offset to our code segment: Far jump!
+
+flush:
     ret
