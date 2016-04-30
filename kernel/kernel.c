a31af13508af6eb4fa559549936c8095bba14138
Ethan R. Jones <doubleplusgood23@gmail.com>
2016-04-30 19:37:58 -0400

    Adding copyrights and proper headers.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,20 @@
+    /* kernel.c - The kernel of project asiago, it's the heart the OS! */
+    /* Copyright (C) 2016   Chris Smith */
+
+    /* This program is free software: you can redistribute it and/or modify */
+    /* it under the terms of the GNU General Public License as published by */
+    /* the Free Software Foundation, either version 3 of the License, or */
+    /* (at your option) any later version. */
+
+    /* This program is distributed in the hope that it will be useful, */
+    /* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+    /* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+    /* GNU General Public License for more details. */
+
+    /* You should have received a copy of the GNU General Public License */
+    /* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+
 #if !defined(__cplusplus)
 #include <stdbool.h> /* C doesn't have booleans by default. */
 #endif
