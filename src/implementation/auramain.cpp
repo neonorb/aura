97114ebb4eddfde1af3a4249ce58f670206fafaa
chris <chris@hichris.com>
2016-06-04 22:59:57 -0400

    Now destroys function before deleting it.

--- src/implementation/auramain.cpp
+++ src/implementation/auramain.cpp
@@ -10,14 +10,11 @@ void auramain() {
 		Function* function = new Function();
 		Bytecode* hi = new Bytecode(PRINTHI);
 		Bytecode* bob = new Bytecode(PRINTBOB);
-		if(hi == NULL){
-			crash("out of memory");
-		}
 		function->bytecodes.add(hi);
 		function->bytecodes.add(bob);
 		execute(function);
 		delete hi;
 		delete bob;
-		delete function;
+		delete function->destroy();
 	}
 }
